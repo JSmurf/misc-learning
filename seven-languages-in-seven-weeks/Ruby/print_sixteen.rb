34be4fb5bc616fc3866bcfe43ccc393911e1bd68
Alex Cooper <mrsketchygalore@me.com>
2018-11-01 14:30:49 -0400

    Update print_sixteen.rb
    Removed some unnecessary code.

--- seven-languages-in-seven-weeks/Ruby/print_sixteen.rb
+++ seven-languages-in-seven-weeks/Ruby/print_sixteen.rb
@@ -2,7 +2,7 @@ numbers = [9,16,22,95,12,23,10,8,93,8,11,16,2,14,12,17]
 
 numbers.each_with_index do |num, i|
     i += 1
-    p numbers[((i-4)...i)] if (i % 4 == 0 and i != 0)
+    p numbers[((i-4)...i)] if i % 4 == 0
 end
 
 puts "Again!" 
